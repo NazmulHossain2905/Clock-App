66f8f704c641ae7db755f15238363e5f660f45ba
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2022-09-19 14:35:01 +0600

    Formate hours 24 to 12

--- App.js
+++ App.js
@@ -31,6 +31,8 @@ function App() {
 
     let amPm = hours < 12 ? 'AM' : 'PM';
 
+    hours = hours > 12 ? hours - 12 : hours;
+
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
@@ -61,12 +63,12 @@ function App() {
   };
 
   React.useEffect(() => {
-    let intervalId = setInterval(() => getTime(), 1000);
-
-    return () => {
-      console.log('Exit ------');
-      clearInterval(intervalId);
-    };
+    setInterval(() => getTime(), 1000);
+    // let intervalId = setInterval(() => getTime(), 1000);
+    // return () => {
+    //   console.log('Exit ------');
+    //   clearInterval(intervalId);
+    // };
   }, []);
 
   return (
